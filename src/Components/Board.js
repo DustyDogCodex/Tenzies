faf68ffd55a314ec86e48b97892e0534eda9cbdc
Varun Malaviya <varunmalaviya7@gmail.com>
2023-01-26 20:19:19 -0500

    moved the dice buttons into its own reusable component. Changed styling to add box shadow to the dice and enlarge font size to make the numbers readable.

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,19 +1,21 @@
+import Dice from "./Dice"
+
 function Board(){
     return(
         <div className="board">
             <h1>Tenzies</h1>
             <p>Roll until all die are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-area">
-                <button className="dice"></button>
-                <button className="dice"></button>
-                <button className="dice"></button>
-                <button className="dice"></button>
-                <button className="dice"></button>
-                <button className="dice"></button>
-                <button className="dice"></button>
-                <button className="dice"></button>
-                <button className="dice"></button>
-                <button className="dice"></button>
+                <Dice value={1}/>
+                <Dice value={1}/>
+                <Dice value={1}/>
+                <Dice value={1}/>
+                <Dice value={1}/>
+                <Dice value={1}/>
+                <Dice value={1}/>
+                <Dice value={1}/>
+                <Dice value={1}/>
+                <Dice value={1}/>
             </div>
             <button className="roll">Roll the dice!</button>
         </div>
---
+++ src/Components/Dice.js
@@ -0,0 +1,7 @@
+function Dice(props) {
+    return(
+        <button className="dice">{props.value}</button>
+    )
+}
+
+export default Dice
