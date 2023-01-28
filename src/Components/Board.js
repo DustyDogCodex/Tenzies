3e72a967cf1a9f75fca4937a295934bc018d0fa4
Varun Malaviya <varunmalaviya7@gmail.com>
2023-01-27 20:19:19 -0500

    added a function to create an array of 10 dice roll values. This array is used to set the state for dice elements which will display a new value once user rolls or resets them.

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,23 +1,31 @@
+import React from "react"
 import Dice from "./Dice"
 
 function Board(){
+    
+    const [diceValues, setDiceValues] = React.useState(newDieRoll())
+
+    function newDieRoll(){
+        const die_array = []
+        for(let i = 0; i < 10; i++){
+            die_array.push(Math.ceil(Math.random() * 6))
+        }
+        return die_array
+    }
+
+    /* array containing 10 dice elements that will be displayed in dice-area div. This way dice elements will load with a new value when user rolls or resets them. */
+    const diceElements = diceValues.map(dice => (
+        <Dice value={dice} />
+    ))
+
     return(
         <div className="board">
             <h1>Tenzies</h1>
             <p>Roll until all die are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-area">
-                <Dice value={1}/>
-                <Dice value={1}/>
-                <Dice value={1}/>
-                <Dice value={1}/>
-                <Dice value={1}/>
-                <Dice value={1}/>
-                <Dice value={1}/>
-                <Dice value={1}/>
-                <Dice value={1}/>
-                <Dice value={1}/>
+                {diceElements}
             </div>
-            <button className="roll">Roll the dice!</button>
+            <button className="roll" onClick={newDieRoll}>Roll the dice!</button>
         </div>
     )
 }
